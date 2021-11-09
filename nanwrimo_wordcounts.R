b27a2fdb1bed5d2500c2311dfb22f12f66966365
Jenn Schilling <jathorne2@gmail.com>
2021-11-08 18:40:22 -0700

    initial commit

---
+++ nanwrimo_wordcounts.R
@@ -0,0 +1,13 @@
+# National Novel Writing Month Word Counts
+# Author: Jenn Schilling
+# November 2021
+
+#### Libraries ####
+
+library(tidyverse)
+library(extrafont)
+library(here)
+
+#### Data ####
+
+word_counts <- read_csv("word_counts.csv")
